6819112a9ae3b2f3fca965802312334213c32058
VictorKoffed <147273918+VictorKoffed@users.noreply.github.com>
2026-08-26 12:13:37 +0200

    Refactor GameStatus enum and comments
    Updated comments to provide English descriptions

--- Hangman.Core/Models/GameStatus.cs
+++ Hangman.Core/Models/GameStatus.cs
@@ -1,12 +1,16 @@
 ﻿/*
-  En enkel enum som håller koll på spelets status:
-  - InProgress = spelet pågår
-  - Won = spelaren vann
-  - Lost = spelaren förlorade
+  Represents the possible states of a game:
+  - InProgress = the game is still active
+  - Won = the player won
+  - Lost = the player lost
+  - Draw = neither player won
 */
 
 namespace Hangman.Core.Models
 {
+    /// <summary>
+    /// Defines the states used to represent the outcome and lifecycle of a game.
+    /// </summary>
     public enum GameStatus
     {
         InProgress,
